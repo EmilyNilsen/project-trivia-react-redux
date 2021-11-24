68f0d094fca3e3df098f87ee38b4486a1676fbfa
RafaelAugustScherer <rafaelaugustscherer@gmail.com>
2021-11-24 14:26:33 -0800

    Adicionado tratamento de questões à página Game

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -11,13 +11,55 @@ class Game extends Component {
 
     this.state = {
       score: 0,
+      questionIndex: 0,
       profilePictureLink: `https://www.gravatar.com/avatar/${MD5(email).toString()}`,
     };
+
+    this.buildAnswersElement = this.buildAnswersElement.bind(this);
+    this.nextQuestion = this.nextQuestion.bind(this);
+  }
+
+  nextQuestion() {
+    this.setState(
+      (prevState) => ({ ...prevState, questionIndex: prevState.questionIndex + 1 }),
+    );
+  }
+
+  buildAnswersElement() {
+    const { nextQuestion } = this;
+    const { questionIndex } = this.state;
+    const { questions } = this.props;
+    const { answers: { correctAnswer, wrongAnswers } } = questions[questionIndex];
+
+    const answers = [
+      <button
+        type="button"
+        onClick={ nextQuestion }
+        key="correct-answer"
+        data-testid="correct-answer"
+      >
+        { correctAnswer }
+      </button>,
+      wrongAnswers.map((answer, idx) => (
+        <button
+          type="button"
+          onClick={ nextQuestion }
+          key={ `wrong-answer-${idx}` }
+          data-testid={ `wrong-answer${idx}` }
+        >
+          { answer }
+        </button>
+      ))];
+    const HALF_RANDOM = 0.5;
+    return answers.sort(() => (Math.random() - HALF_RANDOM));
   }
 
   render() {
-    const { profilePictureLink, score } = this.state;
+    const { buildAnswersElement } = this;
+    const { profilePictureLink, score, questions, questionIndex } = this.state;
     const { name } = this.props;
+
+    const currentQuestion = questions[questionIndex];
 
     return (
       <>
@@ -31,16 +73,19 @@ class Game extends Component {
           <span data-testid="header-score">{ score }</span>
         </header>
         <div>
-          <p>Jogo aqui</p>
+          <p data-testid="question-category">{ currentQuestion.category }</p>
+          <p data-testid="question-text">{ currentQuestion.text }</p>
+          { buildAnswersElement() }
         </div>
       </>
     );
   }
 }
 
-const mapStateToProps = ({ login }) => ({
+const mapStateToProps = ({ login, game }) => ({
   name: login.name,
   email: login.email,
+  questions: game.questions,
 });
 
 export default connect(mapStateToProps)(Game);
@@ -48,4 +93,5 @@ export default connect(mapStateToProps)(Game);
 Game.propTypes = {
   name: PropTypes.string,
   email: PropTypes.string,
+  questions: PropTypes.arrayOf(PropTypes.object),
 }.isRequired;
