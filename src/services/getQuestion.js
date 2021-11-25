e91ef200cc70da714cd950fb9eb7bae901a1b437
Lygia Dias <lygiiadias@gmail.com>
2021-11-24 21:21:04 -0300

    requisiçao API

---
+++ src/services/getQuestion.js
@@ -0,0 +1,9 @@
+const getToken = localStorage.getItem('token;');
+const URL = `https://opentdb.com/api.php?amount=5&token=${getToken}`;
+
+export default async function getQuestion() {
+  const response = await fetch(URL);
+  const JSON = await response.json();
+  const question = JSON.results;
+  return question;
+}
