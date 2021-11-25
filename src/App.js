b2d6f5168fb805796a6ba98ff52dc4d01e77f267
Lygia Dias <lygiiadias@gmail.com>
2021-11-24 21:03:11 -0300

    ok

--- src/App.js
+++ src/App.js
@@ -9,13 +9,10 @@ class App extends Component {
   render() {
     return (
       <Switch>
-<<<<<<< HEAD
-        <Route exact path="/" component={ () => <Login /> } />
+
         <Route exact path="/game" component={ () => <Game /> } />
-=======
         <Route exact path="/" component={ Login } />
         <Route exact patch="/Configuration" component={ Configuration } />
->>>>>>> 001b214fe80bbbb514c3c57683c4594e7eb172e1
       </Switch>
     );
   }
