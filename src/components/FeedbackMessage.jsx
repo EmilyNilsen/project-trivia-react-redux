1d977c18e0845c774d2acb3c82f3b7a077b0e24b
Lygia Dias <lygiiadias@gmail.com>
2021-11-26 15:53:37 -0300

    corrige bug requisito 13

--- src/components/FeedbackMessage.jsx
+++ src/components/FeedbackMessage.jsx
@@ -3,16 +3,21 @@ import React from 'react';
 class FeedbackMessage extends React.Component {
   feedbackMessage() {
     const storage = JSON.parse(localStorage.getItem('state'));
-    const getScore = storage.player.score;
-    const badScore = 3;
+    const getAssertions = storage.player.assertions;
+    const assertions = 3;
 
-    if (getScore < badScore) {
+    if (getAssertions < assertions) {
       return (
         <h1 data-testid="feedback-text">
           Podia ser melhor...
         </h1>);
     }
-    if (getScore >= badScore) return <h1 data-testid="feedback-text">Mandou bem!</h1>;
+    if (getAssertions >= assertions) {
+      return (
+        <h1 data-testid="feedback-text">
+          Mandou bem!
+        </h1>);
+    }
   }
 
   render() {
